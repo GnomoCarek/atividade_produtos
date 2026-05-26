27ccfa58fe876a813f60253e2c0c0221ae97de72
Renan <158374548+GnomoCarek@users.noreply.github.com>
2026-05-26 19:14:09 -0300

    Refactor produtos.h header guard and function signatures

--- produtos.h
+++ produtos.h
@@ -1,4 +1,4 @@
-#ifdef PRODUTOS_H
+#ifndef PRODUTOS_H
 #define PRODUTOS_H
 #define MAX_PRODUTOS 100
 
@@ -8,7 +8,6 @@ struct Produto
     char nome[50];
     float preco;
     int quantidade;
-
 };
 
 void limparTela();
@@ -16,8 +15,7 @@ void pausar();
 void exibirMenu();
 void cadastrarProduto(struct Produto p[],int *qtd);
 void listarProduto(struct Produto p[],int *qtd);
-void calcularEstoque(struct Produto p[],int *qtd);
-void buscarProduto(struct Produto p[],int *qtd,int codigo);
+float calcularEstoque(struct Produto p[],int *qtd);
+int buscarProduto(struct Produto p[],int *qtd,int codigo);
+
 #endif
-
-
